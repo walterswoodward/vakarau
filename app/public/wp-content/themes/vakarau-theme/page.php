9c744c7a27db4daf36fd10c437c4b6d0258f3209
WalterSWoodward <walter.sekope.woodward@gmail.com>
2019-02-17 18:30:43 -0500

    updates page.php

--- app/public/wp-content/themes/vakarau-theme/page.php
+++ app/public/wp-content/themes/vakarau-theme/page.php
@@ -1,10 +1,52 @@
-<!-- If a sinlge.php file exists, then it will be used for all single displays for posts otherwise defaults to index.php -->
+<!-- If a single.php file exists, then it will be used for all single displays for posts otherwise defaults to index.php -->
 <?php
-while(have_posts()) { // Iterates through all your posts
+
+get_header();
+
+while(have_posts()) { // Iterates through all your pages
   the_post(); ?>
-  <h2> This is a page not a post!</h2>
-  <h2><?php the_title(); ?></h2>
-  <?php the_content(); ?>
-  <hr>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">Our History</h1>
+      <div class="page-banner__intro">
+        <p>Learn how the school of your dreams got started.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container container--narrow page-section">
+
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a>
+        <span class="metabox__main">Our History</span></p>
+    </div>
+
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <ul class="min-list">
+        <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li>
+      </ul>
+    </div>
+
+    <div class="generic-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
+        possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
+        consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque
+        vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.
+      </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid
+        possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis,
+        consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque
+        vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.
+      </p>
+    </div>
+
+  </div>
   <?php
 }
+
+get_footer();
+
+?>
