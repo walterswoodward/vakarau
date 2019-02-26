db447930733145234cc7b18126b5fddbf57e785f
WalterSWoodward <walter.sekope.woodward@gmail.com>
2019-02-26 16:16:24 -0500

    page-past events new page created using archive-event as template and query from front-page shows only past events AND contains solution for pagination for custom post types

---
+++ app/public/wp-content/themes/vakarau-theme/page-past-events.php
@@ -0,0 +1,74 @@
+<?php
+get_header();
+?>
+<div class="page-banner">
+  <div class="page-banner__bg-image"
+    style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">
+      Past Events
+    </h1>
+    <div class="page-banner__intro">
+      <p>Listing of all past events</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+  <?php
+   $today = date('Ymd');
+   $pastEvents = new WP_Query(array(
+     'paged' => get_query_var('paged', 1),
+     'posts_per_page' => 1,
+     'post_type' => 'event',
+     'meta_key' => 'event_date',
+     'orderby' => 'meta_value_num',
+     'order' => 'ASC',
+     // Filters out events that took place in the past
+     'meta_query' => array(
+       array(
+         'key' => 'event_date',
+         'compare' => '<',
+         "value" => $today,
+         'type' => 'numeric'
+       )
+     )
+   ));
+  // Iterates over all availables posts using have_posts()
+  while ($pastEvents->have_posts()) {
+    $pastEvents->the_post(); ?>
+  <div class="event-summary">
+    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+      <span
+        class="event-summary__month"><?php
+              // the_field() == echo getfield()
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+           ?></span>
+
+      <span
+        class="event-summary__day"><?php
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('d')
+           ?></span>
+    </a>
+    <div class="event-summary__content">
+      <h5 class="event-summary__title headline headline--tiny"><a
+          href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+      <p><?php echo wp_trim_words(get_the_content(), 12) ?>
+        <a href="<?php the_permalink() ?>" class="nu gray">
+          Learn more
+        </a>
+      </p>
+    </div>
+  </div>
+  <?php
+  }
+  echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+  ));
+  ?>
+</div>
+<?php
+get_footer();
+?>
