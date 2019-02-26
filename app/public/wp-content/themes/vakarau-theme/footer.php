97f2229e1a4fd45a5fa39da29eadc7467d014499
WalterSWoodward <walter.sekope.woodward@gmail.com>
2019-02-26 09:54:13 -0500

    pretty much done with custom post type 'event'

--- app/public/wp-content/themes/vakarau-theme/footer.php
+++ app/public/wp-content/themes/vakarau-theme/footer.php
@@ -6,7 +6,7 @@
 
       <div class="site-footer__col-one">
         <h1 class="school-logo-text school-logo-text--alt-color"><a
-            href="<?php echo site_url('/') ?>"><strong>Vakarau</strong>Industries</a>
+            href="<?php echo site_url('/') ?>"><strong>Vakarau </strong>Industries</a>
         </h1>
         <p><a class="site-footer__link" href="#">555.555.5555</a></p>
       </div>
